70c6f1e83d41d12497e697c2eaa5d05e203abf68
sgiordano45 <notredamegeo@gmail.com>
2025-12-31 18:46:27 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 // service-worker.js - Unified Service Worker
 // Handles both offline functionality AND Firebase Cloud Messaging
-// Version 1.0.19 - firebase-data.js update for submit-statsß
+// Version 1.0.20 - firebase-auth.js update for playee linking and stat aggregation 
 
 
-const CACHE_VERSION = 'aces-v1.0.19';
+const CACHE_VERSION = 'aces-v1.0.20';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
