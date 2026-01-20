46ed76c48f85c4c5116d7a680f72186439b812b1
Stephen Giordano <notredamegeo@gmail.com>
2026-01-20 17:57:46 -0500

    more mobile CSS fixes

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 // service-worker.js - Unified Service Worker
 // Handles both offline functionality AND Firebase Cloud Messaging
-// Version 2.0.2 - mobile-enhacements improvements
+// Version 2.0.3 - mobile-enhacements improvements
 
-const CACHE_VERSION = 'aces-v2.0.2';
+const CACHE_VERSION = 'aces-v2.0.3';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
